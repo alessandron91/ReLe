661253172a8a3cfedbd7a976dce6532f6de528c0
alessandron91 <ale.nuara@gmail.com>
2016-04-07 18:06:33 +0200

    aggiunta classe GQ-Learning

---
+++ rele/include/rele/algorithms/td/GQ-Learning.h
@@ -0,0 +1,86 @@
+/*
+ * rele,
+ *
+ *
+ * Copyright (C) 2015 Davide Tateo & Matteo Pirotta
+ * Versione 1.0
+ *
+ * This file is part of rele.
+ *
+ * rele is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * rele is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with rele.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+#ifndef INCLUDE_RELE_ALGORITHMS_TD_GQ_LEARNING_H_
+#define INCLUDE_RELE_ALGORITHMS_TD_GQ_LEARNING_H_
+
+#include "rele/algorithms/td/Q-Learning.h"
+#include <boost/math/distributions/normal.hpp>
+
+
+namespace ReLe
+{
+
+/*!
+ * This class implements the Weighted Q-Learning algorithm.
+ * This algorithm is an off-policy temporal difference algorithm that,
+ * as Double Q-Learning, tries to solve the well-known overestimation problem suffered
+ * by Q-Learning. This algorithm computes an estimate of the maximum action-value
+ * approximating it as weighted sum of action-values where the weights are the probabilities
+ * of the respective action-value to be the maximum.
+ * While Q-Learning gives the best results when there is a single action-value that is
+ * clearly the maximum and Double Q-Learning gives the best results when all action-values
+ * have almost the same value, Weighted Q-Learning has been proved to work well in intermediate
+ * cases giving, in general, better approximation than the other two approaches.
+ *
+ * References
+ * =========
+ *
+ * ...
+ */
+class GQ_Learning: public Q_Learning
+{
+public:
+    static constexpr double stdZeroValue = 1e-5;
+    static constexpr double stdInfValue = 1e10;
+
+public:
+    GQ_Learning(ActionValuePolicy<FiniteState>& policy, LearningRate& alpha, unsigned int nsamples);
+    virtual void initEpisode(const FiniteState& state, FiniteAction& action) override;
+    virtual void sampleAction(const FiniteState& state, FiniteAction& action) override;
+    virtual void step(const Reward& reward, const FiniteState& nextState,
+                      FiniteAction& action) override;
+    virtual void endEpisode(const Reward& reward) override;
+    virtual void computeWeights(size_t xn);
+
+    virtual ~GQ_Learning();
+
+protected:
+    arma::mat idxs;
+    arma::mat sampleStdQ;
+    arma::mat Q2;
+    arma::mat nUpdates;
+    arma::mat weights;
+    arma::mat weightsVar;
+
+    unsigned int nSamples;
+
+protected:
+    virtual void init() override;
+    inline void updateMeanAndSampleStdQ(double target);
+};
+
+}
+#endif
+
---
+++ rele/src/algorithms/td/GQ-Learning.cpp
@@ -0,0 +1,148 @@
+#include "rele/algorithms/td/GQ-Learning.h"
+#include "rele/utils/RandomGenerator.h"
+using namespace std;
+using namespace arma;
+
+namespace ReLe
+{
+
+GQ_Learning::GQ_Learning(ActionValuePolicy<FiniteState>& policy, LearningRate& alpha, unsigned int nSamples) :
+    Q_Learning(policy, alpha),
+	nSamples(nSamples)
+{
+}
+
+void GQ_Learning::initEpisode(const FiniteState& state, FiniteAction& action)
+{
+    sampleAction(state, action);
+}
+
+void GQ_Learning::sampleAction(const FiniteState& state, FiniteAction& action)
+{
+    x = state.getStateN();
+    u = policy(x);
+
+    action.setActionN(u);
+}
+
+void GQ_Learning::step(const Reward& reward, const FiniteState& nextState,
+                       FiniteAction& action)
+{
+    size_t xn = nextState.getStateN();
+    double r = reward[0];
+
+    computeWeights(xn);
+
+    double W = arma::dot(Q.row(xn), weights.row(xn));
+
+    double target = r + task.gamma * W;
+
+    updateMeanAndSampleStdQ(target);
+
+    x = xn;
+    u = policy(xn);
+
+    action.setActionN(u);
+}
+
+void GQ_Learning::endEpisode(const Reward& reward)
+{
+    double r = reward[0];
+    double target = r;
+
+    updateMeanAndSampleStdQ(target);
+}
+
+GQ_Learning::~GQ_Learning()
+{
+}
+
+void GQ_Learning::init()
+{
+    FiniteTD::init();
+
+    idxs = arma::mat(task.finiteActionDim, task.finiteActionDim - 1, arma::fill::zeros);
+    arma::vec actions = arma::linspace(0, idxs.n_cols, idxs.n_rows);
+    for(unsigned int i = 0; i < idxs.n_rows; i++)
+        idxs.row(i) = actions(arma::find(actions != i)).t();
+
+    sampleStdQ = Q + stdInfValue;
+    Q2 = Q;
+    weights=Q;
+    nUpdates = Q;
+}
+
+inline void GQ_Learning::updateMeanAndSampleStdQ(double target)
+{
+    double alpha = this->alpha(x, u);
+    Q(x, u) = (1 - alpha) * Q(x, u) + alpha * target;
+    Q2(x, u) = (1 - alpha) * Q2(x, u) + alpha * target * target;
+
+    nUpdates(x, u)++;
+
+    if(nUpdates(x, u) > 1)
+    {
+        weightsVar(x, u) = (1 - alpha) * (1 - alpha) * weightsVar(x, u) + alpha * alpha;
+        double n = 1 / weightsVar(x, u);
+
+        double var = (Q2(x, u) - Q(x, u) * Q(x, u)) / n;
+
+        if(var >= stdZeroValue * stdZeroValue)
+            sampleStdQ(x, u) = sqrt(var);
+        else
+            sampleStdQ(x, u) = stdZeroValue;
+    }
+}
+
+void GQ_Learning::computeWeights(size_t xn)
+{
+	arma::mat qSamples=arma::mat(nSamples,Q.n_cols,arma::fill::zeros);
+	arma::vec maxCounter=arma::vec(Q.n_cols,arma::fill::zeros);
+
+	for(int u=0;u<Q.n_cols;u++)
+	{
+		double mean=Q(xn,u);
+		double sigma=sampleStdQ(xn,u);
+		for(int s=0;s<nSamples;s++)
+		{
+			if(sigma>1000)
+			{
+				sigma=100;
+			}
+
+			qSamples(s,u)=RandomGenerator::sampleNormal(mean,sigma);
+		}
+
+	}
+
+	for(int s=0;s<nSamples;s++)
+	{
+		double qmax=qSamples.max();
+		arma::vec samples=qSamples.row(s).t();
+		arma::uvec maxIndex = find(samples == qmax);
+		unsigned int index = RandomGenerator::sampleUniformInt(0,
+		                         maxIndex.n_elem - 1);
+
+		maxCounter(maxIndex(index))++;
+
+
+
+
+	}
+
+	for(unsigned u=0;u<Q.n_cols;u++)
+	{
+		weights(xn,u)=maxCounter(u)/nSamples;
+	}
+
+
+
+
+
+
+
+
+
+}
+
+}
